f7df5caacba115b37c416c238a37e0232246b405
Jose Cisneros <joseacisnerosm@gmail.com>
2023-09-19 04:01:30 -0600

    init motion integration

--- controllers/opponent/opponent.py
+++ controllers/opponent/opponent.py
@@ -15,38 +15,17 @@
 """Minimalist controller example for the Robot Wrestling Tournament.
    Demonstrates how to play a simple motion file."""
 
-from controller import Robot
-import sys
-sys.path.append('..')
+from controller import Robot, Motion
 
-from utils.image_processing import ImageProcessing as IP
-from utils.fall_detection import FallDetection
-from utils.gait_manager import GaitManager
-from utils.camera import Camera
-from utils.sonar import Sonar
-from utils.move_routine import MoveRoutine
 
 class Wrestler (Robot):
-    def __init__(self):
-        Robot.__init__(self)
-        self.time_step = int(self.getBasicTimeStep())
-        self.camera = Camera(self)
-        self.fall_detector = FallDetection(self.time_step, self)
-        self.gait_manager = GaitManager(self, self.time_step)
-        self.sonar = Sonar(self, self.time_step)
-
     def run(self):
-        self.singleMovemement = MoveRoutine(self.time_step, self)
-        print("Test in opponent")
-
-        self.singleMovemement.exec()
-
-        # motion = Motion('../motions/GetUp2.motion')  # look into this text file, it's easy to understand
-        # motion.setLoop(True)
-        # motion.play()
-        # time_step = int(self.getBasicTimeStep())  # retrieves the WorldInfo.basicTimeTime (ms) from the world file
-        # while self.step(self.time_step) != -1:  # runs the hand wave motion in a loop until Webots quits
-        #     pass
+        motion = Motion('../motions/HandWave.motion')  # look into this text file, it's easy to understand
+        motion.setLoop(True)
+        motion.play()
+        time_step = int(self.getBasicTimeStep())  # retrieves the WorldInfo.basicTimeTime (ms) from the world file
+        while self.step(time_step) != -1:  # runs the hand wave motion in a loop until Webots quits
+            pass
 
 
 # create the Robot instance and run main loop
