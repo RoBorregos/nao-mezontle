f643add857d256db8ab9a67d595d32fc5964c69f
Francisco-SP3 <a01177893@tec.mx>
2023-09-26 02:01:29 -0600

    Punch changes

--- utils/py_motions/arms_up.py
+++ utils/py_motions/arms_up.py
@@ -3,39 +3,110 @@ def arms_up():
 	times = list()
 	keys = list()
 
-	times_v = [0.0, 0.2, 0.4]
+	times_v = [0.0, 0.05, 0.1, 0.15, 0.2]
 
 	names.append("RShoulderRoll")
 	times.append(times_v)
-	keys.append([-0.4, -0.4, -0.4])
+	keys.append([-0.4, -0.4, 0.0, 0.0, -0.4])
 
 	names.append("LShoulderRoll")
 	times.append(times_v)
-	keys.append([0.4, 0.1, 0.4])
+	keys.append([0.4, 0.4, 0.0, 0.0, 0.4])
 
 	names.append("RShoulderPitch")
 	times.append(times_v)
-	keys.append([0.7, -1.5, 0.7])
+	keys.append([0.7, 0.7, 0.0, 0.0, 0.7])
 
 	names.append("LShoulderPitch")
 	times.append(times_v)
-	keys.append([0.7, -1.5, 0.7])
+	keys.append([0.7, 0.7, 0.0, 0.0, 0.7])
 
 	names.append("RElbowRoll")
 	times.append(times_v)
-	keys.append([0.8, 0.8, 0.8])
+	keys.append([0.8, 0.8, 0.0, 0.0, 0.8])
 
 	names.append("LElbowRoll")
 	times.append(times_v)
-	keys.append([-0.8, -0.8, -0.8])
+	keys.append([-0.8, -0.8, 0.0, 0.0, -0.8])
 
 	names.append("RElbowYaw")
 	times.append(times_v)
-	keys.append([0.0, 0.0, 0.0])
+	keys.append([0.0, 0.0, 0.0, 0.0, 0.0])
 
 	names.append("LElbowYaw")
 	times.append(times_v)
-	keys.append([0.0, 0.0, 0.0])
+	keys.append([0.0, 0.0, 0.0, 0.0, 0.0])
 
+	names.append("RWristYaw")
+	times.append(times_v)
+	keys.append([0.0, 0.0, 1.8, 1.8, 0.0])
+
+	names.append("LWristYaw")
+	times.append(times_v)
+	keys.append([0.0, 0.0, 1.8, 1.8, 0.0])
+
+	names.append("LPhalanx1")
+	times.append(times_v)
+	keys.append([0.0, 1, 1, 1, 0.0])
+	
+	names.append("RPhalanx1")
+	times.append(times_v)
+	keys.append([0.0, 1, 1, 1, 0.0])
+
+	names.append("LPhalanx2")
+	times.append(times_v)
+	keys.append([0.0, 1, 1, 1, 0.0])
+
+	names.append("RPhalanx2")
+	times.append(times_v)
+	keys.append([0.0, 1, 1, 1, 0.0])
+
+	names.append("LPhalanx3")
+	times.append(times_v)
+	keys.append([0.0, 1, 1, 1, 0.0])
+
+	names.append("RPhalanx3")
+	times.append(times_v)
+	keys.append([0.0, 1, 1, 1, 0.0])
+    
+	names.append("LPhalanx4")
+	times.append(times_v)
+	keys.append([0.0, 1, 1, 1, 0.0])
+
+	names.append("RPhalanx4")
+	times.append(times_v)
+	keys.append([0.0, 1, 1, 1, 0.0])
+
+	names.append("LPhalanx5")
+	times.append(times_v)
+	keys.append([0.0, 1, 1, 1, 0.0])
+
+	names.append("RPhalanx5")
+	times.append(times_v)
+	keys.append([0.0, 1, 1, 1, 0.0])
+
+	names.append("LPhalanx6")
+	times.append(times_v)
+	keys.append([0.0, 1, 1, 1, 0.0])
+
+	names.append("RPhalanx6")
+	times.append(times_v)
+	keys.append([0.0, 1, 1, 1, 0.0])
+    
+	names.append("LPhalanx7")
+	times.append(times_v)
+	keys.append([0.0, 1, 1, 1, 0.0])
+
+	names.append("RPhalanx7")
+	times.append(times_v)
+	keys.append([0.0, 1, 1, 1, 0.0])
+
+	names.append("LPhalanx8")
+	times.append(times_v)
+	keys.append([0.0, 1, 1, 1, 0.0])
+
+	names.append("RPhalanx8")
+	times.append(times_v)
+	keys.append([0.0, 1, 1, 1, 0.0])
 
 	return names, times, keys
