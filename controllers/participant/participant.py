bab6ca63fdbfd884bfeda1adf520d10226efda6d
Alejandra Coeto <a01285221@tec.mx>
2023-09-26 03:32:00 -0600

    fak

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # comment de la suerte 1
-#fak
+# asi se queda
 
 """
 Demonstrates the gait manager (inverse kinematics + simple ellipsoid path).
